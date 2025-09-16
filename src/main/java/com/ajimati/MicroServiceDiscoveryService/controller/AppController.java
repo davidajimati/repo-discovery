4248e952c58f6a2a85dd3f44a242936e14c3bdec
David Ajimati <david.ajimati@stanbicibtc.com>
2025-09-16 15:46:17 +0100

    allowed all origins

--- src/main/java/com/ajimati/MicroServiceDiscoveryService/controller/AppController.java
+++ src/main/java/com/ajimati/MicroServiceDiscoveryService/controller/AppController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
